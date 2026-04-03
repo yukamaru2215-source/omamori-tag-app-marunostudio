bf7db4e192b9a68ee8e2dcfdc54063bdaaf7143b
yukamaru2215 <yukamaru2215@gmail.com>
2026-04-03 15:38:45 +0900

    fix: restore lib folder

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,2 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-};
-
-export default nextConfig;
+const nextConfig = {}
+export default nextConfig
